8408c3cd8e8dddb07cef13d3daddd0338305b9a6
Mark Yen <mark.yen@suse.com>
2021-10-06 18:44:14 -0700

    Remove ldconfig hacks, use wsl.conf to turn it off.
    While it's not documented, it appears to be possible to turn off init
    running ldconfig by setting a wsl.conf option.  The description of this
    option was noted in the file generated.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- build.sh
+++ build.sh
@@ -55,7 +55,4 @@ for field in $(awk -F= '/=/{ print $1 }' /os-release); do
 done
 
 # Configuration for WSL compatibility
-mkdir -p /distro/etc/ld.so.conf.d
 cp wsl.conf /distro/etc/wsl.conf
-printf "#!/bin/sh\nexec /bin/busybox true" > /distro/sbin/ldconfig
-chmod u+x /distro/sbin/ldconfig
